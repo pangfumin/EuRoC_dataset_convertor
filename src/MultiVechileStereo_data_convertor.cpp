2153ca86e74b8d4dbff62c94ab8a2dbea81b8123
pangfumin <pangfumin@gmai.com>
2019-06-25 11:01:15 +0800

    add MultiVechileStereo

---
+++ src/MultiVechileStereo_data_convertor.cpp
@@ -0,0 +1,306 @@
+/*********************************************************************************
+ *  OKVIS - Open Keyframe-based Visual-Inertial SLAM
+ *  Copyright (c) 2015, Autonomous Systems Lab / ETH Zurich
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of Autonomous Systems Lab / ETH Zurich nor the names of
+ *     its contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Created on: Jan 13, 2016
+ *      Author: Stefan Leutenegger (s.leutenegger@imperial.ac.uk)
+ *********************************************************************************/
+
+/**
+ * @file dataset_convertor.cpp
+ * @brief Source file for the VioParametersReader class.
+ * @author Stefan Leutenegger
+ * @author Andrea Nicastro
+ */
+
+#include <vector>
+#include <map>
+#include <memory>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#pragma GCC diagnostic ignored "-Woverloaded-virtual"
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <rosbag/chunked_file.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#pragma GCC diagnostic pop
+
+
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <boost/foreach.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
+#include "okvis/file-system-tools.h"
+#include <pcl/io/pcd_io.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+
+using namespace boost::filesystem;
+using std::vector;
+using std::string;
+using std::map;
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::shared_ptr;
+
+const string RESET = "\033[0m";
+const string BLACK = "0m";
+const string RED = "1m";
+const string GREEN = "2m";
+const string BOLD = "\033[1;3";
+const string REGULAR = "\033[0;3";
+const string UNDERLINE = "\033[4;3";
+const string BACKGROUND = "\033[4";
+
+const int DOUBLE_PRECISION = 17;
+
+
+
+string colouredString(string str, string colour, string option)
+{
+  return option + colour + str + RESET;
+}
+
+
+int writePointCloudToBinFile(pcl::PointCloud<pcl::PointXYZI>& pointcloud, const std::string bin_filename) {
+    // load point cloud
+    std::ofstream output(bin_filename, std::ios::out | std::ios::binary);
+    if(!output.is_open()){
+        std::cerr << "Could not read file: " << bin_filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    for (auto i : pointcloud) {
+        output.write((char *) &i.x, 3*sizeof(float));
+        output.write((char *) &i.intensity, sizeof(float));
+    }
+
+    output.close();
+    return pointcloud.size();
+}
+
+
+int main(int argc, char **argv)
+{
+
+  cout << colouredString("Initializing ROS node:", RED, BOLD) << endl;
+
+  ros::init(argc, argv, "dataset_converter");
+
+  ros::NodeHandle nh;
+
+  cout << colouredString("DONE!", GREEN, BOLD) << endl;
+
+  cout << colouredString("Initializing sensor information:", RED, BOLD) << endl;
+
+  string path(argv[1]);
+  size_t pos = path.find_last_of("/");
+  size_t pos_dot = path.find_last_of(".");
+
+  string bagname;
+  string bagpath;
+  if (pos == string::npos) {
+    cout
+        << colouredString(
+            "Relative path are not supported. Use an absolute path instead."
+            "For example: roslaunch okvis_ros convert_datasert.launch bag:= /absolute/path/here",
+            RED, BOLD) << endl;
+    exit (EXIT_FAILURE);
+  } else {
+    bagname = path.substr(pos + 1, pos_dot - pos - 1);
+    bagpath = path.substr(0, pos + 1);
+  }
+
+  cout << colouredString("Reading bag:", RED, BOLD) << endl;
+
+  std::string save_folder = bagpath + bagname;
+  auto velodyne_folder = save_folder+ "/velodyne";
+  auto image_left_folder = save_folder+ "/image0";
+  auto image_right_folder = save_folder+ "/image1";
+  if (!common::pathExists(save_folder)) {
+      common::createPath(save_folder);
+  }
+
+    if (!common::pathExists(velodyne_folder)) {
+        common::createPath(velodyne_folder);
+    }
+    if (!common::pathExists(image_left_folder)) {
+        common::createPath(image_left_folder);
+    }
+    if (!common::pathExists(image_right_folder)) {
+        common::createPath(image_right_folder);
+    }
+
+    std::string velodyne_list_file = save_folder+ "/velodyne.txt";
+    std::string left_image_list_file = save_folder+ "/image0.txt";
+    std::string right_image_list_file = save_folder+ "/image1.txt";
+
+    std::ofstream velodyne_ofs(velodyne_list_file);
+    std::ofstream left_image_ofs(left_image_list_file);
+    std::ofstream right_image_ofs(right_image_list_file);
+
+
+
+
+    rosbag::Bag bag;
+  cout << colouredString("\tOpening bag...", RED, REGULAR);
+  bag.open(argv[1], rosbag::bagmode::Read);
+  cout << colouredString("\t[DONE!]", GREEN, REGULAR) << endl;
+
+  cout << colouredString("\tQuering topics bag...", RED, REGULAR);
+
+  vector < string > topic_list;
+
+  rosbag::View view(bag);
+
+  vector<const rosbag::ConnectionInfo *> bag_info = view.getConnections();
+  std::set < string > bag_topics;
+
+  for (const rosbag::ConnectionInfo *info : bag_info) {
+    string topic_name;
+    topic_name = info->topic;
+
+    std::cout << "topic: " << topic_name << std::endl;
+
+    topic_list.push_back(topic_name);
+  }
+
+  view.addQuery(bag, rosbag::TopicQuery(topic_list));
+
+  cout << colouredString("\t[DONE!]", GREEN, REGULAR) << endl;
+
+  cout << colouredString("\tOpening file streams...", RED, REGULAR);
+
+  double view_size = view.size();
+  std::cout << "view_size: " << view_size << std::endl;
+  double counter = 0;
+  int velodyne_points_cnt = 0;
+  int camera_left = 0;
+  int camera_right = 0;
+
+  for (auto bagIt : view) {
+    string topic = bagIt.getTopic();
+
+    if (topic == "/velodyne_point_cloud" ) {
+      // todo: save
+        sensor_msgs::PointCloud2::ConstPtr pointcloud =
+                bagIt.instantiate<sensor_msgs::PointCloud2>();
+        pcl::PointCloud<pcl::PointXYZI>::Ptr lidar(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::PCLPointCloud2 pcl_pc2;
+        pcl_conversions::toPCL(*pointcloud,pcl_pc2);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr out_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
+        for (auto pt: *temp_cloud) {
+            pcl::PointXYZI p;
+            p.x = pt.x;
+            p.y = pt.y;
+            p.z = pt.z;
+            out_cloud->push_back(p);
+        }
+
+        std::string lidar_name = std::string(6 - std::to_string(velodyne_points_cnt).length(), '0') + std::to_string(velodyne_points_cnt) + ".bin";
+        std::stringstream ss;
+        ss <<velodyne_folder << "/" << lidar_name;
+
+        velodyne_ofs << std::to_string(bagIt.getTime().toNSec()) << " " << "velodyne/"<< lidar_name << std::endl;
+
+        writePointCloudToBinFile(*out_cloud, ss.str());
+
+      velodyne_points_cnt ++;
+    }
+
+    if (topic == "/visensor/left/image_raw" ) {
+      sensor_msgs::Image::ConstPtr image =
+              bagIt.instantiate<sensor_msgs::Image>();
+
+      cv_bridge::CvImagePtr cv_ptr;
+      cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+      cv::Mat image_cv = cv_ptr->image;
+//      cv::imshow("image left", image_cv);
+//      cv::waitKey(20);
+
+        std::string image_name = std::string(6 - std::to_string(camera_left).length(), '0') + std::to_string(camera_left) + ".png";
+        std::stringstream ss;
+        ss <<image_left_folder << "/" << image_name;
+        cv::imwrite(ss.str(), image_cv);
+
+      left_image_ofs << std::to_string(bagIt.getTime().toNSec()) << " " << "image0/"<< image_name << std::endl;
+      camera_left ++;
+    }
+
+    if (topic == "/visensor/right/image_raw" ) {
+
+      sensor_msgs::Image::ConstPtr image =
+              bagIt.instantiate<sensor_msgs::Image>();
+
+      cv_bridge::CvImagePtr cv_ptr;
+      cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+      cv::Mat image_cv = cv_ptr->image;
+//      cv::imshow("image right", image_cv);
+//      cv::waitKey(20);
+
+
+        std::string image_name = std::string(6 - std::to_string(camera_right).length(), '0') + std::to_string(camera_right) + ".png";
+        std::stringstream ss;
+        ss <<image_right_folder << "/" << image_name;
+        cv::imwrite(ss.str(), image_cv);
+
+        right_image_ofs << std::to_string(bagIt.getTime().toNSec()) << " " << "image1/"<< image_name << std::endl;
+
+      camera_right ++;
+    }
+
+    std::cout<< "saving: " << camera_right + camera_left + velodyne_points_cnt << "/"  << view_size << std::endl;
+
+  }
+
+    velodyne_ofs.close();
+    left_image_ofs.close();
+    right_image_ofs.close();
+
+
+
+
+  ros::shutdown();
+
+}
---
+++ src/MultiVechileStereo_data_convertor_syn.cpp
@@ -0,0 +1,144 @@
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <fstream>
+#include <opencv2/highgui/highgui.hpp>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/io/pcd_io.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+
+#include "okvis/file-system-tools.h"
+using namespace sensor_msgs;
+using namespace message_filters;
+
+std::string save_folder = "/home/pang/data/dataset/multi_srereo/outdoor_day1_data";
+auto image0_syn_folder = save_folder+ "/image0_syn";
+auto image1_syn_folder = save_folder+ "/image1_syn";
+auto lidar_syn_folder = save_folder+ "/velodyne_syn";
+auto sensor_syn_file = save_folder+ "/sensor_syn.txt";
+
+std::ofstream sensor_syn_ofs;
+int sensor_cnt = 0;
+
+int writePointCloudToBinFile(pcl::PointCloud<pcl::PointXYZI>& pointcloud, const std::string bin_filename) {
+    // load point cloud
+    std::ofstream output(bin_filename, std::ios::out | std::ios::binary);
+    if(!output.is_open()){
+        std::cerr << "Could not read file: " << bin_filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    for (auto i : pointcloud) {
+        output.write((char *) &i.x, 3*sizeof(float));
+        output.write((char *) &i.intensity, sizeof(float));
+    }
+
+    output.close();
+    return pointcloud.size();
+}
+
+void callback(const ImageConstPtr& image0, const ImageConstPtr& image1, const PointCloud2ConstPtr& lidar)
+{
+    // Solve all of perception here...
+    int64_t  dt0 = (int64_t)(image0->header.stamp.toNSec() - image1->header.stamp.toNSec());
+    int64_t  dt1 = (int64_t)(image0->header.stamp.toNSec() - lidar->header.stamp.toNSec());
+
+//    std::cout << dt0 << " " << dt1 << std::endl;
+
+    // in 5 ms, save it
+    if (abs(dt0) < 5 * 1e6 && abs(dt1) < 30 * 1e6) {
+
+        cv_bridge::CvImagePtr left_cv_ptr;
+        left_cv_ptr = cv_bridge::toCvCopy(image0, sensor_msgs::image_encodings::BGR8);
+        cv::Mat left_image_cv = left_cv_ptr->image;
+
+        cv_bridge::CvImagePtr right_cv_ptr;
+        right_cv_ptr = cv_bridge::toCvCopy(image1, sensor_msgs::image_encodings::BGR8);
+        cv::Mat right_image_cv = right_cv_ptr->image;
+
+
+        std::string image_name = std::string(6 - std::to_string(sensor_cnt).length(), '0') + std::to_string(sensor_cnt) + ".png";
+        std::stringstream ss0, ss1;
+        ss0 <<image0_syn_folder << "/" << image_name;
+        cv::imwrite(ss0.str(), left_image_cv);
+
+        ss1 <<image1_syn_folder << "/" << image_name;
+        cv::imwrite(ss1.str(), right_image_cv);
+
+//        pcl::PointCloud<pcl::PointXYZI>::Ptr lidar(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::PCLPointCloud2 pcl_pc2;
+        pcl_conversions::toPCL(*lidar.get(),pcl_pc2);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr out_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
+        for (auto pt: *temp_cloud) {
+            pcl::PointXYZI p;
+            p.x = pt.x;
+            p.y = pt.y;
+            p.z = pt.z;
+            out_cloud->push_back(p);
+        }
+
+        std::string lidar_name = std::string(6 - std::to_string(sensor_cnt).length(), '0') + std::to_string(sensor_cnt) + ".bin";
+        std::stringstream ss_lidar;
+        ss_lidar <<lidar_syn_folder << "/" << lidar_name;
+
+        writePointCloudToBinFile(*out_cloud, ss_lidar.str());
+
+
+        sensor_syn_ofs  << "image0_syn/"<< image_name << " "
+                        << std::to_string(image0->header.stamp.toNSec()) << " "
+                << "image1_syn/"<< image_name << " "
+                << std::to_string(image1->header.stamp.toNSec()) << " "
+                << "velodyne_syn/"<< lidar_name << " "
+                << std::to_string(lidar->header.stamp.toNSec()) << " "
+                << dt0 << " " << dt1
+                <<  std::endl;
+
+        std::cout << image_name << " "
+                << image0->header.stamp.toNSec() << " "
+                << image1->header.stamp.toNSec() << " "
+                << lidar->header.stamp.toNSec() << std::endl;
+
+        sensor_cnt ++;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "vision_node");
+
+    ros::NodeHandle nh;
+    message_filters::Subscriber<Image> image0_sub(nh, "/visensor/left/image_raw", 10);
+    message_filters::Subscriber<Image> image1_sub(nh, "/visensor/right/image_raw", 10);
+    message_filters::Subscriber<PointCloud2> lidar_sub(nh, "/velodyne_point_cloud", 10);
+
+    if (!common::pathExists(image0_syn_folder)) {
+        common::createPath(image0_syn_folder);
+    }
+    if (!common::pathExists(image1_syn_folder)) {
+        common::createPath(image1_syn_folder);
+    }
+
+    if (!common::pathExists(lidar_syn_folder)) {
+        common::createPath(lidar_syn_folder);
+    }
+
+    sensor_syn_ofs.open(sensor_syn_file);
+
+    typedef sync_policies::ApproximateTime<Image, Image, PointCloud2> MySyncPolicy;
+    // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
+    Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), image0_sub, image1_sub, lidar_sub);
+    sync.registerCallback(boost::bind(&callback, _1, _2, _3));
+
+    ros::spin();
+
+    return 0;
+}
